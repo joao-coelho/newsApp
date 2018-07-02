fd66e8ff324252a55bb38585474d9f67a8b10214
joao-coelho <coelhox_bonyz@hotmail.com>
2018-07-02 14:02:37 +0100

    Added Subscriptions page

---
+++ src/components/Subscriptions.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="subscriptions">
+    <b-container fluid>
+      <navbar-feed></navbar-feed>
+      <b-row>
+        <sidebar-menu :user_categories="categories"></sidebar-menu>
+        <div id="subscriptions-section">
+          <b-table hover :items="items" :fields="fields"></b-table>
+        </div>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import NavbarFeed from './NavbarFeed'
+import SidebarMenu from './SidebarMenu'
+
+export default {
+  name: 'Subscriptions',
+  components: {
+    NavbarFeed,
+    SidebarMenu
+  },
+  data() {
+    return {
+      categories: [
+        "business",
+        "sports",
+        "health",
+        "science",
+        "economy",
+        "politics"
+      ],
+      fields: {
+        name: {
+          label: this.nameLabel,
+          sortable: true
+        },
+        description: {
+          label: this.descriptionLabel,
+          sortable: false
+        },
+        categories: {
+          label: this.categoriesLabel,
+          sortable: true
+        }
+      },
+      items: [
+        {
+          name: "Channel Name",
+          description: "Channel Description, a long one, reaaaaally long, like huge, yap that big biiiiiiiiiiiiitx",
+          categories: "Sports, Business"
+        },
+        {
+           name: "Channel Name",
+           description: "Channel Description, a long one, reaaaaally long, like huge, yap that big biiiiiiiiiiiiitx",
+           categories: "Sports, Business"
+         }
+      ]
+    }
+  },
+  computed: {
+    nameLabel() {
+      return this.$t('subscriptions_nameIcon');
+    },
+    descriptionLabel() {
+      return this.$t('subscriptions_channelDescription');
+    },
+    categoriesLabel() {
+      return this.$t('subscriptions_channelCategories');
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+    var menu = document.getElementsByClassName("sidebar-menu-list")[0];
+    var $li = menu.firstChild.childNodes;
+    $li[4].classList.add("active");
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+@import "styles/global.scss";
+
+#subscriptions-section {
+  padding: 2rem 4rem;
+  margin-top: 50px;
+  margin-left: 225px;
+  width: calc(100% - 225px);
+  @media (min-width: $break-large) {
+    padding: 2rem 7rem;
+  }
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Feed from '@/components/Feed'
 import Login from '@/components/Login'
 import Register from '@/components/Register'
+import Subscriptions from '@/components/Subscriptions'
 
 Vue.use(Router)
 
@@ -23,6 +24,11 @@ export default new Router({
       path: '/register',
       name: 'Register',
       component: Register
+    },
+    {
+      path: '/subscriptions',
+      name: 'Subscriptions',
+      component: Subscriptions
     }
   ]
 })
